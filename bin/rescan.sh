e231a367381ded726cec4fef7be41c9b6f0c44b8
Evan Summers <evan.summers@gmail.com>
2017-03-10 09:00:10 +0000

    publish

--- bin/rescan.sh
+++ bin/rescan.sh
@@ -3,7 +3,7 @@ set -u -e
 
 if which node7 >/dev/null
 then 
-  node7 --harmony lib/index.js
+  node7 --harmony ~/node_modules/rescan/lib/index.js
 elif ! which node
 then
   exit 1
@@ -12,6 +12,6 @@ then
   node -v
   exit 1
 else
-  node --harmony lib/index.js
+  node --harmony ~/node_modules/rescan/lib/index.js
 fi
 
